341c39f4fe597b5b398dd1bbc3f324f434f1fbfa
WUYUEZONG <w.yzong@outlook.com>
2022-09-30 10:57:53 +0800

    1

--- iOS16OrientationDemo/ViewController.m
+++ iOS16OrientationDemo/ViewController.m
@@ -7,7 +7,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scroller;
+@property (weak, nonatomic) IBOutlet UIView *videoView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *top;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottom;
+@property (weak, nonatomic) IBOutlet UIButton *changeOrientations;
 
 @end
 
@@ -16,7 +21,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _scroller.maximumZoomScale = 1.7;
+}
+- (IBAction)gotoChangeScreenAction:(UIButton *)sender {
+    
+    sender.selected = !sender.isSelected;
+    
+    [self setNeedsUpdateOfSupportedInterfaceOrientations];
+    NSArray *array = [[[UIApplication sharedApplication] connectedScenes] allObjects];
+    UIWindowScene *scene = [array firstObject];
+    UIWindowSceneGeometryPreferencesIOS *pIOS = [[UIWindowSceneGeometryPreferencesIOS alloc] init];
+    if (sender.isSelected) {
+        // to full screen
+        pIOS.interfaceOrientations = UIInterfaceOrientationMaskLandscapeRight;
+            
+    } else {
+        pIOS.interfaceOrientations = UIInterfaceOrientationMaskPortrait;
+    }
+    [scene requestGeometryUpdateWithPreferences:pIOS errorHandler:^(NSError * _Nonnull error) {
+    }];
+    
+    
 }
 
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    
+    NSLog(@"size width: %f, height: %f", size.width, size.height);
+    _changeOrientations.selected = size.width > size.height;
+    if (size.width > size.height) {
+        _top.constant = 0;
+        _bottom.active = true;
+        
+    } else {
+        _top.constant = 20;
+        _bottom.active = false;
+    }
+    
+}
+
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return _videoView;
+}
 
 @end
